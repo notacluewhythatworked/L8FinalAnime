b100e6b0ec91005b8e43b15206ae188bf8e37307
notacluewhythatworked <73045785+notacluewhythatworked@users.noreply.github.com>
2020-12-01 17:51:42 -0700

    Added new classes

---
+++ app/src/main/java/com/example/l8anime/Genre_Scifi.java
@@ -0,0 +1,4 @@
+package com.example.l8anime;
+
+public class Genre_Scifi {
+}
---
+++ app/src/main/java/com/example/l8anime/Recommendations.java
@@ -0,0 +1,14 @@
+package com.example.l8anime;
+
+public class Recommendations {
+    private Genre_Action[] action;
+    private Genre_Comedy[] comedy;
+    private Genre_Drama[] drama;
+    private Genre_Horror[] horror;
+    private Genre_Mystery[] mystery;
+    private Genre_Romance[] romance;
+    private Genre_SchoolLife[] schoolLife;
+    private Genre_Scifi[] scifi;
+
+
+}
