231bae20df3ea8d550e22750ec6f1fc78f47a48d
notacluewhythatworked <73045785+notacluewhythatworked@users.noreply.github.com>
2020-12-02 16:12:13 -0700

    Drafted a UI and wrote instantiating code

--- app/src/main/java/com/example/l8anime/MainActivity.java
+++ app/src/main/java/com/example/l8anime/MainActivity.java
@@ -3,12 +3,36 @@ package com.example.l8anime;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
+    private Switch action;
+    private Switch comedy;
+    private Switch drama;
+    private Switch horror;
+    private Switch mystery;
+    private Switch romance;
+    private Switch schoolLife;
+    private Switch scifi;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        action = findViewById(R.id.actionSwitch);
+        comedy = findViewById(R.id.comedySwitch);
+        drama = findViewById(R.id.dramaSwitch);
+        horror = findViewById(R.id.horrorSwitch);
+        mystery = findViewById(R.id.mysterySwitch);
+        romance = findViewById(R.id.romanceSwitch);
+        schoolLife = findViewById(R.id.schoolLifeSwitch);
+        scifi = findViewById(R.id.scifiSwitch);
+    }
+
+    public void onSubmitClick(View view){
+
     }
 }
